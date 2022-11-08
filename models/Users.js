9287e018f369be67756de40dd6714e4877df8d88
Toacin <toacinp@outlook.com>
2022-11-07 17:21:09 -0700

    user model complete

--- models/Users.js
+++ models/Users.js
@@ -2,6 +2,33 @@ const {Schema, model} = require('mongoose');
 
 const usersSchema = new Schema(
     {
-        
+        username: {type: String, unique: "Username already taken", required: "username is required", trim: true},
+        email: {type: String, unique: "email already taken", required: "email is required"},
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
     }
 )
+
+usersSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const Users = model('users', usersSchema);
+
+module.exports = Users;
