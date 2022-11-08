262d50ed836ffc16f63cbdb17b9d3f28c1f099ba
Toacin <toacinp@outlook.com>
2022-11-07 17:40:25 -0700

    thoughts model complete

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,1 +1,29 @@
 const {Schema, model} = require('mongoose');
+
+const thoughtsSchema = new Schema(
+    {
+        thoughtText: {type: String, required: "text is required", minlength: 1, maxlength: 280},
+        createdAt: {type: Date, default: Date.now, get: (timestamp) => dateFormat(timestamp)},
+        username: {type: String, required: "username is required"},
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'reactions'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+)
+
+thoughtsSchema.virtual('reactionsCount').get(function() {
+    return this.reactions.length;
+})
+
+const Thoughts = model('thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
